ad59641b37aa20a7e74b9ed3cff7b1b424e48073
Gabriel H. Maia <maiamaiagabriel@gmail.com>
2016-11-12 09:11:30 -0200

    Primero dia

--- app/arrays.js
+++ app/arrays.js
@@ -2,17 +2,37 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum: function(arr) {
-
+    var sum = 0;
+    arr.forEach(function (item) {
+      sum += item;
+    });
+    return sum;
   },
 
   remove: function(arr, item) {
 
-  },
+    for (var i = 0; i <= arr.length; i++){
+      if (arr[i] == item) {
+        if (arr[i] == arr[i + 1]){
+          arr.slice(arr[i], 2);
+        }else{
+          arr.slice(arr[i], 1);
+        }
+      }
+    }
 
+    // arr.forEach(function (forItem) {
+    //   if (item === forItem){
+    //     var index = arr.indexOf(item);
+    //     if (index > -1) arr.splice(index, 1);
+    //   }
+    // });
+    return arr;
+  },
   removeWithoutCopy: function(arr, item) {
 
   },
